a87149f60a96a893044f3739b801053569082697
Anna Murga <nmurga0304@gmail.com>
2024-06-05 09:41:10 +0300

    Added tests for API GitHub (Emojis and Commits)

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -16,3 +16,18 @@ class GitHub:
 
         return body
     
+    def get_emojis(self):
+        r = requests.get('https://api.github.com/emojis')
+        body = r.json()
+
+        return body
+    
+    def get_list_commit(self, owner, repo):
+        r = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits')
+
+        return r
+    
+    def get_list_branches_for_HEAD_commit(self, owner, repo, commit_sha):
+        r = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits/{commit_sha}/branches-where-head')
+
+        return r
